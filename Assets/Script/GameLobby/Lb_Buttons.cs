3ec20f756d41344b9b6f8db34ef029bde721a776
pernandoang <102746832+pernandoang@users.noreply.github.com>
2024-06-23 20:53:43 +0700

    add and remove stamina

---
+++ Assets/Script/GameLobby/Lb_Buttons.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Lb_Buttons : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+    [SerializeField]
+    private float Stamina = 300;
+    private void Update()
+    {
+        var clampStm = Math.Clamp(Stamina, 0f, 300f);
+        Text.text = clampStm.ToString();
+    }
+    public void AddStamina()
+    {
+        var CurrentStm = Stamina;
+        Stamina = Math.Clamp(CurrentStm + 10,0f,300f);
+        Debug.Log(Stamina);
+    }
+    public void RemoveStamina() 
+    {
+        var CurrentStm = Stamina;
+        Stamina = Math.Clamp(CurrentStm - 10, 0f, 300f);
+        Debug.Log(Stamina);
+    }
+}
